fa4aa3346638c1fd92473020235cf9ccb04687e9
fangjun <fjun99@gmail.com>
2022-02-14 23:34:18 +0800

    solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,12 +28,12 @@ import("./tasks/mytask");
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+       process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
