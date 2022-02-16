e184e4efb0571d113b2913cd4afce63e15b2bbaa
fangjun <fjun99@gmail.com>
2022-02-16 13:59:15 +0800

    modi

--- contracts/ClassOwnToken.sol
+++ contracts/ClassOwnToken.sol
@@ -5,14 +5,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ClassOwnToken is ERC20,Ownable {
-        constructor(uint256 initialSupply) ERC20("ClassOwnToken", "COT") {
-        _mint(msg.sender, initialSupply);
-        }
-
+        
         event Mint(address mintTo, uint amount);
 
-        function mint(address mintTo,uint amount) public onlyOwner{
-                require(mintTo == address(mintTo),"Invalid address");
+        constructor(uint256 initialSupply) 
+          ERC20("ClassOwnToken", "COT") {
+                _mint(msg.sender, initialSupply);
+        }
+
+        function mint(address mintTo,uint amount) 
+          public onlyOwner{
+                require(mintTo == address(mintTo),
+                        "Invalid address");
                 require(amount > 0,"Amount should > 0");
                 _mint(mintTo, amount);
                 emit Mint(mintTo,amount);
--- contracts/ClassToken.sol
+++ contracts/ClassToken.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ClassToken is ERC20 {
-        constructor(uint256 initialSupply) ERC20("ClassToken", "CLT") {
-        _mint(msg.sender, initialSupply);
+        constructor(uint256 initialSupply) 
+          ERC20("ClassToken", "CLT") 
+        {
+                _mint(msg.sender, initialSupply);
         }
 }
